1ee829ecfba1d22c0a064e05ffbd6738f4528a1c
mohadesea <amanzadehmohadese@gmail.com>
2025-03-09 14:55:39 +0330

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule , DialogModule],
+  imports: [RouterOutlet, ButtonModule , DialogModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
